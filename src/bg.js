8d3e8722ab3d989c318b3e0d46c9b1fbf3690d0f
Hans Meyer <git@hansmeyer.com>
2015-12-14 16:32:39 -0700

    eliminate "Close tab" entry from browser_action context menu

--- src/bg.js
+++ src/bg.js
@@ -1,14 +1,15 @@
 /* global chrome:true, contextMenuLabel:true, get:true */
 
 chrome.contextMenus.create({
-    id: 'shortcut-key',
-    title: contextMenuLabel(get('shortcut-key')),
-    contexts: ['all'],
-    onclick: function(info, t) {
-        chrome.tabs.remove(t.id);
-    }
+	id: 'shortcut-key',
+	title: contextMenuLabel(get('shortcut-key')),
+	// contexts: ['all'],
+	contexts: ['page', 'frame', 'selection', 'link', 'editable', 'image', 'video', 'audio'], //  eliminate 'browser_action', 'page_action'
+	onclick: function(info, t) {
+		chrome.tabs.remove(t.id);
+	}
 });
 
 chrome.browserAction.onClicked.addListener(function(t) {
-    chrome.tabs.remove(t.id);
+	chrome.tabs.remove(t.id);
 });
