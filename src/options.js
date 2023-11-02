ecc38120108d02cfc707467d1488bd2fdea96f94
Hans Meyer <hansifer@users.noreply.github.com>
2023-11-02 12:28:19 -0600

    formatting

--- src/options.js
+++ src/options.js
@@ -1,28 +1,26 @@
 /* global chrome:true, contextMenuLabel:true, get:true, set:true */
 
-(function(doc) {
+(function (doc) {
+  chrome.runtime.getPlatformInfo(function (info) {
+    // todo: support for other OS's
+    if (info.os === 'mac') {
+      doc.getElementById('shortcut-key-not-supported').style.display = '';
+    } else {
+      doc.getElementById('shortcut-key-supported').style.display = '';
 
-	chrome.runtime.getPlatformInfo(function(info) {
-		// todo: tests for other OS's
-		if (info.os === 'mac') {
-			doc.getElementById('shortcut-key-not-supported').style.display = '';
-		} else {
-			doc.getElementById('shortcut-key-supported').style.display = '';
+      var shortcutKey = doc.getElementById('shortcut-key');
 
-			var shortcutKey = doc.getElementById('shortcut-key');
+      shortcutKey.addEventListener('change', function () {
+        chrome.contextMenus.update('shortcut-key', {
+          title: contextMenuLabel(shortcutKey.value),
+        });
 
-			shortcutKey.addEventListener('change', function() {
-				chrome.contextMenus.update('shortcut-key', {
-					title: contextMenuLabel(shortcutKey.value)
-				});
+        set('shortcut-key', shortcutKey.value);
+      });
 
-				set('shortcut-key', shortcutKey.value);
-			});
+      shortcutKey.value = get('shortcut-key');
 
-			shortcutKey.value = get('shortcut-key');
-
-			shortcutKey.focus();
-		}
-	});
-
-}(document));
+      shortcutKey.focus();
+    }
+  });
+})(document);
