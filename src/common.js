f8cbc384017ccdaf290da55f0d050a8c9fb5906a
Hans Meyer <hansifer@users.noreply.github.com>
2023-11-02 12:28:48 -0600

    formatting; export functions

--- src/common.js
+++ src/common.js
@@ -1,41 +1,41 @@
 /* exported contextMenuLabel, get, set */
 
-function contextMenuLabel(shortcutKey) {
-    switch (shortcutKey) {
-        case 'W':
-        case 'Q':
-        case 'X':
-        case 'E':
-        case 'D':
-        case 'Z':
-            return 'Close tab  (&' + shortcutKey + ')';
-    }
+export function contextMenuLabel(shortcutKey) {
+  switch (shortcutKey) {
+    case 'W':
+    case 'Q':
+    case 'X':
+    case 'E':
+    case 'D':
+    case 'Z':
+      return 'Close tab  (&' + shortcutKey + ')';
+  }
 
-    return '&Close tab';
+  return '&Close tab';
 }
 
-function get(key) {
-    var val = localStorage.getItem(key);
+export function get(key) {
+  var val = localStorage.getItem(key);
 
-    if (val == null && key === 'shortcut-key') {
-        val = localStorage.getItem('shortcutKey');
-    }
+  if (val == null && key === 'shortcut-key') {
+    val = localStorage.getItem('shortcutKey');
+  }
 
-    if (val) {
-        return val;
-    }
+  if (val) {
+    return val;
+  }
 
-    // return default
-    switch (key) {
-        case 'shortcut-key':
-            return 'C';
-    }
+  // return default
+  switch (key) {
+    case 'shortcut-key':
+      return 'C';
+  }
 }
 
-function set(key, val) {
-    if (key === 'shortcut-key') {
-        localStorage.removeItem('shortcutKey');
-    }
+export function set(key, val) {
+  if (key === 'shortcut-key') {
+    localStorage.removeItem('shortcutKey');
+  }
 
-    localStorage.setItem(key, val);
+  localStorage.setItem(key, val);
 }
