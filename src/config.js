51dd7eb2f70bbb9facbeb17dbef9ac22b7d04bcf
Hans Meyer <ham@hansifer.com>
2023-11-03 16:16:22 -0600

    add config

---
+++ src/config.js
@@ -0,0 +1,2 @@
+export const DEFAULT_SHORTCUT_KEY = 'C';
+export const OTHER_SHORTCUT_KEYS = ['W', 'Q', 'X', 'E', 'D', 'Z'];
---
+++ src_manifest_v2/config.js
@@ -0,0 +1,2 @@
+const DEFAULT_SHORTCUT_KEY = 'C';
+const OTHER_SHORTCUT_KEYS = ['W', 'Q', 'X', 'E', 'D', 'Z'];
