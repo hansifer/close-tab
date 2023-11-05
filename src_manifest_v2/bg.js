43e24ff37331b6dc98e20c3abf3b083b9de2d878
Hans Meyer <hansifer@users.noreply.github.com>
2023-11-05 11:41:48 -0700

    add context menu asynchronously

--- src_manifest_v2/bg.js
+++ src_manifest_v2/bg.js
@@ -2,49 +2,43 @@ const MENU_ID = 'close-tab';
 
 // ------------------------ HANDLE ACTION ------------------------
 
-chrome.contextMenus.create({
-  id: MENU_ID,
-  title: getMenuItemLabel(DEFAULT_SHORTCUT_KEY),
-  // contexts: ['all'],
-  contexts: [
-    'page',
-    'frame',
-    'selection',
-    'link',
-    'editable',
-    'image',
-    'video',
-    'audio',
-  ], //  eliminate 'browser_action', 'page_action'
-  onclick: (_, tab) => {
-    chrome.tabs.remove(tab.id);
-  },
-});
-
 chrome.browserAction.onClicked.addListener((tab) => {
   chrome.tabs.remove(tab.id);
 });
 
-// --------------------- SET MENU ITEM LABEL ---------------------
+getShortcutKey().then((shortcutKey) => {
+  // ------------------------ HANDLE ACTION ------------------------
 
-chrome.storage.onChanged.addListener((changes, areaName) => {
-  if (areaName === 'sync') {
-    const newShortcutKey = changes.shortcutKey?.newValue;
-    if (newShortcutKey) updateMenuItemLabel(newShortcutKey);
-  }
+  chrome.contextMenus.create({
+    id: MENU_ID,
+    title: getMenuItemLabel(shortcutKey),
+    // contexts: ['all'],
+    contexts: [
+      'page',
+      'frame',
+      'selection',
+      'link',
+      'editable',
+      'image',
+      'video',
+      'audio',
+    ], //  eliminate 'browser_action', 'page_action'
+    onclick: (_, tab) => {
+      chrome.tabs.remove(tab.id);
+    },
+  });
+
+  // --------------------- SET MENU ITEM LABEL ---------------------
+
+  chrome.storage.onChanged.addListener((changes, areaName) => {
+    if (areaName === 'sync') {
+      const newShortcutKey = changes.shortcutKey?.newValue;
+      if (newShortcutKey) updateMenuItemLabel(newShortcutKey);
+    }
+  });
 });
 
-initMenuItemLabel();
-
 // ----------------------------- UTIL -----------------------------
-
-async function initMenuItemLabel() {
-  const shortcutKey = await getShortcutKey();
-
-  if (shortcutKey !== DEFAULT_SHORTCUT_KEY) {
-    updateMenuItemLabel(shortcutKey);
-  }
-}
 
 function updateMenuItemLabel(key) {
   chrome.contextMenus.update(MENU_ID, {
